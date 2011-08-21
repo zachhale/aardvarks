6be2d47d3fb0f60ea68bc06283be319633f303cc
Jay Coskey <jay.coskey@gmail.com>
2011-08-21 05:07:48 -0700

    Have wave use proper shape (slim rectangle facing direction of velocity) for "collision" detection.

--- Classes/Wave.m
+++ Classes/Wave.m
@@ -6,12 +6,14 @@
 //  Copyright 2011 Aardvarks. All rights reserved.
 //
 
+#import "math.h"
 #import "Wave.h"
 
 #define ELASTICITY 1.0f
 #define FRICTION   0.0f
 #define MASS       1.0f
 #define THICKNESS  20.0f
+
 
 @implementation Wave
 
@@ -24,7 +26,7 @@
 	imageView.transform = CGAffineTransformMakeTranslation(body.pos.x - THICKNESS, body.pos.y - THICKNESS);
 }
 
-- (id)initWithPosition:(cpVect)position Velocity:(cpVect)velocity
+- (id)initWithPosition:(cpVect)position Dimensions:(cpVect)paddleDimensions Velocity:(cpVect)velocity
 {
 	if(self = [super init])
 	{
@@ -38,15 +40,37 @@
 		cpVect offset;
 		offset.x = 0;
 		offset.y = 0;
-		
-		cpFloat moment = cpMomentForCircle(mass, 0, THICKNESS, offset);
+        
+        cpVect hDims = cpvmult(paddleDimensions, 0.5);
+        
+        float velocityNorm = sqrt(velocity.x * velocity.x + velocity.y * velocity.y);
+        cpVect unitVel = cpvmult(velocity, 1 / velocityNorm);
+                
+        cpVect verts[4];
+        verts[0] = cpv(     hDims.x,      hDims.y);
+        verts[1] = cpv(     hDims.x, -1 * hDims.y);
+        verts[2] = cpv(-1 * hDims.x, -1 * hDims.y);
+        verts[3] = cpv(-1 * hDims.x,      hDims.y);
+        
+        cpVect rotVerts[4];
+        // TODO: Refactor this repetitive code to a matrix multiplication function.
+        rotVerts[0] = cpv(   unitVel.y * verts[0].x + unitVel.x * verts[0].y,
+                        -1 * unitVel.x * verts[0].x + unitVel.y * verts[0].y);
+        rotVerts[1] = cpv(unitVel.y * verts[1].x + unitVel.x * verts[1].y,
+                        -1 * unitVel.x * verts[1].x + unitVel.y * verts[1].y);
+        rotVerts[2] = cpv(unitVel.y * verts[2].x + unitVel.x * verts[2].y,
+                        -1 * unitVel.x * verts[2].x + unitVel.y * verts[2].y);
+        rotVerts[3] = cpv(unitVel.y * verts[3].x + unitVel.x * verts[3].y,
+                        -1 * unitVel.x * verts[3].x + unitVel.y * verts[3].y);
+
+		cpFloat moment = cpMomentForPoly(mass, 3, rotVerts, position);
 		
 		body = [[ChipmunkBody alloc] initWithMass:mass andMoment:moment];
 		body.pos = position;
 		body.vel = velocity;
-		
-		ChipmunkShape *shape = [ChipmunkCircleShape circleWithBody:body radius:THICKNESS offset:offset];
-		
+
+		ChipmunkPolyShape * shape = [ChipmunkPolyShape polyWithBody:body count:4 verts:rotVerts offset:position];
+
 		// So it will bounce forever
 		shape.elasticity = ELASTICITY;
 		shape.friction = FRICTION;
